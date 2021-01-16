a4d69b80cbf645a5a9ded38874cc7892cf55a433
Sebastian Safari <ssebs@ymail.com>
2021-01-15 19:31:10 -0800

    wpn switching working, but collision masks are messed up

--- Scenes/Guns/TestGun.gd
+++ Scenes/Guns/TestGun.gd
@@ -1,5 +1,4 @@
-extends Spatial
-
+extends RigidBody
 
 onready var bullet_scn = preload("res://Scenes/Bullet.tscn")
 onready var fp = $FirePoint
@@ -9,14 +8,25 @@ var ammo_left = 10
 var ammo_spare = 20
 var max_ammo_in_mag = 10
 
+var dropped = false
+
+
+func _process(delta):
+	if dropped:
+		# Drop it forward
+		self.apply_impulse(self.transform.basis.z, -transform.basis.z * 10)
+		dropped = false
+
 func shoot():
 	if ap.is_playing():
 		return
+	
 	
 	var b = bullet_scn.instance()
 	fp.add_child(b)
 	b.global_transform = self.global_transform
 	b.DAMAGE = 50
-	b.SPEED = 2
-	
+	b.SPEED = 10
+
 	ap.play("Fire")
+# shoot
--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -11,14 +11,18 @@ var jump = 5
 var direction: Vector3
 var velocity: Vector3
 var fall: Vector3
+var gun
+var wpn_to_spawn
+var wpn_to_drop
 
 onready var cam = $Head/Camera
 onready var head = $Head
 onready var hand = $Head/Hand
 onready var crosshair = $Head/Camera/TextureRect
-onready var gun = $Head/Hand/TestGun
+onready var reach = $Head/Camera/Reach
 
-# for mouse input
+
+# For mouse input
 func _input(event):
 	# Click to capture mouse
 	if Input.is_mouse_button_pressed(BUTTON_LEFT) and not get_is_mouse_captured():
@@ -37,10 +41,26 @@ func _input(event):
 # _input
 
 func _process(delta):
-	if show_crosshair:
-		crosshair.show()
+	crosshair.show() if show_crosshair else crosshair.hide()
+	
+	# Choose gun to pickup
+	if reach.is_colliding() and reach.get_collider():
+		if reach.get_collider().has_method("shoot"):
+			wpn_to_spawn = load(reach.get_collider().filename).instance()
+#			print(wpn_to_spawn)
+		else:
+			wpn_to_spawn = null
 	else:
-		crosshair.hide()
+		wpn_to_spawn = null
+	
+	# Choose a gun to drop
+	if hand.get_child(0) != null:
+		wpn_to_drop = load(hand.get_child(0).filename).instance()
+#		print(wpn_to_drop)
+	else:
+		wpn_to_drop = null
+	
+# _process
 
 func _physics_process(delta):
 	handle_movement(delta)
@@ -55,7 +75,7 @@ func handle_movement(delta):
 		fall.y -= GRAVITY * delta
 	
 	# Jump
-	if Input.is_action_pressed("jump"):
+	if Input.is_action_pressed("jump") and is_on_floor():
 		fall.y = jump
 	
 	# Movement
@@ -76,12 +96,26 @@ func handle_movement(delta):
 
 func handle_guns(delta):
 	if Input.is_action_just_pressed("fire"):
-		gun.shoot()
+		# Gun is here
+		if gun:
+			gun.shoot()
+
+	# Pickup and drop gun
+	if Input.is_action_just_pressed("interact"):
+		if wpn_to_spawn != null:
+			if hand.get_child(0) != null:
+				self.get_parent().add_child(wpn_to_drop)
+				wpn_to_drop.global_transform = hand.global_transform
+				wpn_to_drop.dropped = true
+				hand.get_child(0).queue_free()
+			reach.get_collider().queue_free()
+			hand.add_child(wpn_to_spawn)
+			wpn_to_spawn.rotation = hand.rotation
+			gun = wpn_to_spawn
 # handle_guns
 
 
 # Helper functions
-
 func get_is_mouse_captured() -> bool:
 	return Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
 # get_is_mouse_captured
